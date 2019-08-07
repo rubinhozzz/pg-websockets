ce6310d34ff51a0b0713eaba060f27c0f9d6b91c
rubinhozzz <ruben.flex@gmail.com>
2019-08-07 16:48:27 +0200

    function and trigger for notifying table to clients

--- db.sql
+++ db.sql
@@ -6,3 +6,17 @@ CREATE TABLE todo
     description character varying(255),
     done boolean NOT NULL
 )
+
+CREATE OR REPLACE FUNCTION notify_alert_insert() RETURNS trigger AS $$
+DECLARE
+BEGIN
+  PERFORM pg_notify('watchers_' || NEW.id, row_to_json(NEW)::TEXT);
+  RETURN new;
+END;
+$$ LANGUAGE plpgsql;
+
+
+DROP TRIGGER IF EXISTS trig_alert_insert ON todo;
+
+CREATE TRIGGER trig_alert_insert AFTER INSERT ON todo
+FOR EACH ROW EXECUTE PROCEDURE notify_alert_insert();
